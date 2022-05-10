700456325a166ee8c047e563c4d2c87447bdf0de
JTDoe <Jordendoerflinger@gmail.com>
2022-05-09 21:11:24 -0500

    all but move piece

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ const rl = readline.createInterface({
 });
 
 // An object that represents the three stacks of Towers of Hanoi; 
-  // * each key is an array of Numbers: 
-    // * A is the far-left, 
-    // * B is the middle, 
-    // * C is the far-right stack
-      // * Each number represents the largest to smallest tokens: 
-        // * 4 is the largest, 
-        // * 1 is the smallest
+// * each key is an array of Numbers: 
+// * A is the far-left, 
+// * B is the middle, 
+// * C is the far-right stack
+// * Each number represents the largest to smallest tokens: 
+// * 4 is the largest, 
+// * 1 is the smallest
 
 let stacks = {
   a: [4, 3, 2, 1],
@@ -32,32 +32,44 @@ const printStacks = () => {
 // Next, what do you think this function should do?
 const movePiece = (startStack, endStack) => {
   // Your code here
-let lastItem = stacks[startStack].pop()
-stacks[endStack].push(lastItem)
+  let lastItem = stacks[startStack].pop();
+  stacks[endStack].push(lastItem)
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
+const isLegal = (startStack, endStack) => {
   // Your code here
-// if statement that ensures only smaller pieces are moved on top of larger pieces or empty row.
-
-
-
+  // if statement that ensures only smaller pieces are moved on top of larger pieces or empty row.
+  if (stacks[endStack].length == 0) {
+    return true
+  }
+  if (stacks[startStack].slice(-1) < stacks[endStack].slice(-1)) {
+    return true
+  }
+  else return false
 }
 
 // What is a win in Towers of Hanoi? When should this function run?
 const checkForWin = () => {
   // Your code here
-if (stacks = { a: [], b: [4, 3, 2, 1], c: [] }) {
-  return true
-} else {
-  return false
-}
+  // If statement to determine if all four pieces were moved onto another peg in correct order.
+  if (stacks.b.length == 4 || stacks.c.length == 4) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // When is this function called? What should it do with its argument?
+// this function should be called if a legal move is made and it should move a piece.
 const towersOfHanoi = (startStack, endStack) => {
   // Your code here
+  if (isLegal(startStack, endStack)) {
+    movePiece()
+  }
+  if (checkForWin()) {
+    return true
+  }
 
 }
 
